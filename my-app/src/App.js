177ddfc1b327dc3e16087d993f5aff7729f47b26
esparsh287 <esparsh.tamrakar18@gmail.com>
2024-10-02 09:38:04 +0545

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+//import About from './components/About';
+/*import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  
+} from "react-router-dom";*/
+
+function App() {
+  const [mode, setMode]=useState('light');
+  const [alert, setAlert] = useState(null);
+  const showAlert=(message, type)=>{
+      setAlert({
+        msg:message,
+        type: type
+      });
+      setTimeout(()=>{
+        setAlert(null)
+      }, 2000)
+  }
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark')
+      document.body.style.backgroundColor='#042743';
+      showAlert("Dark Mode Enabled", "success")
+    }else{
+      setMode('light')
+      document.body.style.backgroundColor='white';
+      showAlert("Light Mode Enabled", "success")
+    }
+  }
+  return (
+    <>
+    
+    <Navbar title="Text Utils" aboutText="About Us" mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
+    <div className="container my-3">
+    
+          <TextForm heading="Enter text to analyze" mode={mode} showAlert={showAlert} />
+
+    </div>
+
+
+    </>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/TextForm.js
@@ -0,0 +1,73 @@
+import React, {useState} from 'react'
+
+
+export default function TextForm(props) {
+  const handleUpClick=()=>{
+    let newText= text.toUpperCase();
+    setText(newText)
+    props.showAlert("Converted to UpperCase!", "success")
+  }
+
+  const handleOnChange=(event)=>{
+    setText(event.target.value)
+  }
+
+  const handleLowpClick=()=>{
+    let newText=text.toLowerCase();
+    setText(newText)
+    props.showAlert("Converted to LowerCase!", "success")
+  }
+
+  const handleColorClick=()=>{
+    let c= Math.random()
+    let newColor
+    if(c<=1/3){
+      newColor = 'blue'
+    }else if(c>1/3 && c<2/3){
+      newColor= 'red'
+    }else{
+      newColor='black'
+    }
+    setColor(newColor)
+    props.showAlert("Changed Color", "success")
+  }
+  const handleClear=()=>{
+    let newText=''
+    setText(newText)
+    props.showAlert("Cleared Text", "success")
+  }
+  const handleCopy=()=>{
+    let text=document.getElementById('myBox')
+    text.select()
+    navigator.clipboard.writeText(text.value)
+    props.showAlert("Copied to Clipboard", "success")
+  }
+  const [text, setText]= useState('');
+  const [color, setColor]= useState('black');
+  
+  
+  return (
+    <>
+    <div className='container' style={{color: props.mode==='dark'? 'white': 'black'}}>
+          <h1>{props.heading} </h1>
+          <div className="mb-3">
+          <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange} style={{color:color, backgroundColor:props.mode==='dark' ?'grey':'white'}}></textarea>
+    </div>
+      <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="btn btn-primary mx-2" onClick={handleLowpClick}>Convert to Lowercase</button>
+      <button className="btn btn-primary mx-2" onClick={handleColorClick}>Change Color</button>
+      <button className="btn btn-primary mx-2" onClick={handleClear}>Clear Text</button>
+    </div>
+    <div className="my-2 mx-3">
+    <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
+    </div>
+    <div className="container my-2" style={{color: props.mode==='dark'? 'white': 'black'}}>
+      <h1>Your text summary</h1>
+      <p>{text.split(/\s+/).filter((element) => element.length !== 0).length} Words and {text.length} characters</p>
+      <p>{0.008 * text.split(/\s+/).filter((element) => element.length !== 0).length} <b>minutes-Time to read</b></p>
+      <h3>Preview</h3>
+      <p>{text.length>0? text: 'Enter something above to preview'}</p>
+    </div>
+    </>
+  )
+}
